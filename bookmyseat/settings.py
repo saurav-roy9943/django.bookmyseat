1436298334ec79b940cc40053372f873f84c31bd
saurav-GLA <saurav.rai@gla.ac.in>
2025-07-03 07:32:28 +0530

    first commit

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-^ie+vijh@f^_en*54!^&*ua=h85ob1cwe+47drtku-!gkwt=tk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
